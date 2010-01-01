27c3ca7ded9d57f214f5dcc63e013b0a6fc19e1f
Will Koffel <wkoffel@alum.mit.edu>
2010-01-01 16:12:23 -0500

    changed method names to be less likely to conflict

--- lib/action_controller/acts/xmlrpc_endpoint.rb
+++ lib/action_controller/acts/xmlrpc_endpoint.rb
@@ -7,7 +7,7 @@ module ActionController
 
       module ClassMethods
         def exposes_xmlrpc_methods(options = {})
-          configuration = { :method_prefix => nil, :endpoint_action => "index" }
+          configuration = { :method_prefix => nil }
           configuration.update(options) if options.is_a?(Hash)
           
           before_filter(:add_method_handlers, :only => [:index])
@@ -15,7 +15,7 @@ module ActionController
             require 'xmlrpc/server'
             include ActionController::Acts::XmlrpcEndpoint::InstanceMethods
 
-            def method_prefix
+            def xe_method_prefix
               '#{configuration[:method_prefix]}'
             end
           EOV
@@ -23,9 +23,8 @@ module ActionController
       end
 
       module InstanceMethods
-        # TODO: name this via the endpoint_action option instead of hardcoding to "index"
         # TODO: add route automatically for this?
-        def index
+        def xe_index
           result = @xmlrpc_server.process(request.body)
           puts "\n\n----- BEGIN RESULT -----\n#{result}----- END RESULT -----\n\n"
           render :text => result, :content_type => 'text/xml'
@@ -37,9 +36,9 @@ module ActionController
           @xmlrpc_server = XMLRPC::BasicServer.new
           # loop through all the methods, adding them as handlers
           self.class.instance_methods(false).each do |method|
-            unless ['index'].member?(method)
+            unless ['xe_index', 'xe_method_prefix'].member?(method)
               puts "Adding XMLRPC method for #{method.to_s}"
-              @xmlrpc_server.add_handler(method_prefix + method) do |*args|
+              @xmlrpc_server.add_handler(xe_method_prefix + method) do |*args|
                 self.send(method.to_sym, *args)
               end
             end
